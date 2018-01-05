0f535ef3caf16f36b70d9b74213dadbf44e3aef4
Shawn Chen <ethanloatrchen@gmail.com>
2018-01-04 18:58:19 -0800

    use hash route

--- src/router.js
+++ src/router.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 
 // Components
 import Header from './components/header';
