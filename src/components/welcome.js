c81a5dbbb95d4f212194dfb59271b7bb86af548c
Shawn Chen <ethanloatrchen@gmail.com>
2018-01-14 09:15:00 -0800

    create room dialog

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import { connect } from "react-redux";
 import { withRouter, Link } from 'react-router-dom';
 import _ from 'lodash';
-import { Toolbar, Button, MenuButton, Drawer, List, ListItem, Divider, Avatar, FontIcon, Subheader } from 'react-md';
+import { DialogContainer, TextField, Button } from 'react-md';
 import db from '../utilities/db';
 
 class Welcome extends Component {
   constructor(props){
     super(props);
-    this.state = {room: null};
+    this.state = { room: null, createVisible:false };
   }
   crateRoom(){
     let userName = 'user name' || this.state.name;
@@ -26,7 +26,10 @@ class Welcome extends Component {
         name: userName,
         num: 1
       }]
-    });
+    }, () => this.showCreate(false));
+  }
+  showCreate(createVisible){
+    this.setState({createVisible});
   }
   render(){
     return (
@@ -36,8 +39,24 @@ class Welcome extends Component {
         <p class="text-center">Enter the room on the left side</p>
         <h4 class="text-center">OR</h4>
         <p class="text-center">
-          <Button flat primary iconClassName="fa fa-plus" onClick={()=>this.crateRoom()}>Create a new room</Button>
+          <Button flat primary iconClassName="fa fa-plus" onClick={()=>this.showCreate(true)}>Create a new room</Button>
         </p>
+        <DialogContainer
+          id="create-new-room-dialog"
+          actions={[
+            { secondary: true, children: 'Cancel', onClick: ()=>this.showCreate(false) },
+            { primary: true, children: 'Confirm', onClick: ()=>this.crateRoom() }
+          ]}
+          visible={this.state.createVisible}
+          onHide={()=>this.showCreate(false)}
+          title="Create a new room">
+          <TextField required
+            id="create-new-room-name"
+            label="Room Name"
+            onChange={(e)=>this.editText('roomname', e)}
+            placeholder="Enter room name"
+          />
+        </DialogContainer>
       </div>
     )
   }
